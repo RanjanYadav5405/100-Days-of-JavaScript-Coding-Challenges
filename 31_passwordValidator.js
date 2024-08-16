914ac3a527d25de9a5a0a37fe18e6672c8d130cf
Ranjan Yadav <ranjanyadav5405@gmail.com>
2024-08-17 00:06:21 +0530

    password validator

--- 31_passwordValidator.js
+++ 31_passwordValidator.js
@@ -1,14 +1,13 @@
-const passwordValidator = (password) =>{
-    if(password.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*_)(?!.*\W)(?!.* ).{8,16}$/g)){
-        return true;
-    }
-    return false;
+const passwordValidator = (password) => {
+    // if (password.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*_)(?!.*\W)(?!.* ).{8,16}$/g)) {
+    //     return true;
+    // }
+    // return false;
+
+    return /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*_)(?!.*\W)(?!.* ).{8,16}$/g.test(password)
 }
 
-
-
-
 console.log(passwordValidator("wiohniewudfh")) // false
-console.log(passwordValidator("wiohniewudfh1A")) // true
-console.log(passwordValidator("wiohniewudfh"))  // false
-console.log(passwordValidator("wiohniewudfh243BDG")) // true
+console.log(passwordValidator("wiohniewudfh1A")) // false (no underscore)
+console.log(passwordValidator("wiohniewudfh_1A"))  // true
+console.log(passwordValidator("wiohniewudfh243BDG")) // false (no underscore)

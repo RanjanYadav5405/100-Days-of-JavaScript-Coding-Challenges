3bbf9f9da5759ce68bee1ca0bc448df4139223ee
Ranjan Yadav <ranjanyadav5405@gmail.com>
2024-08-18 00:48:08 +0530

    codebytes

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,4 +1,12 @@
- for(let i = 1; i<=arr[0];i++){
-//      result+="*";
-//  }
-//  return result
+let rev=""
+    // for(let i = newStr.length-1 ; i >=0; i--){
+    //     rev +=newStr[i]
+    // }
+    // return rev
+
+    // let doubNum = rev.map((curElem, index) => {
+    //      if(index % 2 === 0){
+    //         curElem = curElem * 2;
+    //      }
+    // })
+    // console.log(doubNum)
